fee2e90219ef13d1b1b2dd44e3c109aafd506a2e
nika <vvvv127@gmail.com>
2026-03-03 18:33:38 -0800

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my view</title>
+</head>
+<body>
+    <x-layout>
+        <x-slot:title>
+            {{$title}}-site.com
+        </x-slot>
+
+        {{$text}}
+    <x-layout>
+</body>
+</html>
+
+<?php
+class PostController extends Controller{
+    public function show(){
+        return view('post.show',[
+            'title'=>'page title',
+            'text'=>'page content',
+        ]);
+    }
+}
+?>
